d79c13b2570d25e907bef74841d5efec6247717e
Mary <maryag@email.unc.edu>
2021-01-28 17:55:39 -0500

    quiz2

--- exercises/Quiz.py
+++ exercises/Quiz.py
@@ -1,15 +1,7 @@
-"""Unstopable Gamestop (stock symbol $GME)"""
-gme: float = 30.0
-print("01/13: " + str(gme))
-fund_short: float = gme
-reddit_share: float = gme
+"""XYZ 123."""
 
-gme = gme + 130.0
-print("01/26: " + str(gme))
-print("fund: " + str(fund_short - gme))
-print("reddit: " + str(gme - reddit_share))
-
-gme = 200.0 + gme
-print("01/27: " + str(gme))
-print("fund: " + str(fund_short - gme))
-print("reddit: " + str(gme - reddit_share))
+a: str = "x"
+b: str = "y" + a + "y"
+c: str = "z" + b + "z"
+n: int = len(c)
+print(c[n - 1])
