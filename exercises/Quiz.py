a45104aa26e8fc4262f2b892c93aa3d9b91f9edf
Mary <maryag@email.unc.edu>
2021-01-28 17:39:49 -0500

    Quiz

---
+++ exercises/Quiz.py
@@ -0,0 +1,15 @@
+"""Unstopable Gamestop (stock symbol $GME)"""
+gme: float = 30.0
+print("01/13: " + str(gme))
+fund_short: float = gme
+reddit_share: float = gme
+
+gme = gme + 130.0
+print("01/26: " + str(gme))
+print("fund: " + str(fund_short - gme))
+print("reddit: " + str(gme - reddit_share))
+
+gme = 200.0 + gme
+print("01/27: " + str(gme))
+print("fund: " + str(fund_short - gme))
+print("reddit: " + str(gme - reddit_share))
