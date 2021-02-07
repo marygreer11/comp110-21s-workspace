af761a7c57083826dd85f635bae32a7857b79afd
Mary <maryag@email.unc.edu>
2021-02-06 23:02:41 -0500

    EX01 Changes

--- exercises/ex01/tar_heels.py
+++ exercises/ex01/tar_heels.py
@@ -5,10 +5,10 @@ __author__ = "730394883"
 
 x: int = int(input("Enter an int: "))
 
-if x % 2== 0:
+if x % 2 == 0:
     print("TAR")
 else:
-    if x % 7== 0:
+    if x % 7 == 0:
         print("HEELS")
     else:
         print("CAROLINA")
--- exercises/ex01/vaccine_calc.py
+++ exercises/ex01/vaccine_calc.py
@@ -1,6 +1,6 @@
 """A vaccination calculator."""
 
-__author__ = "YOUR PID HERE"
+__author__ = "730394883"
 
 # The datetime data type is imported from the datetime library.
 # A datetime object models a specific date and time.
@@ -17,4 +17,13 @@ from datetime import datetime
 from datetime import timedelta
 
 
-# Begin your solution here...
+Population: int = int(input("The total number of people under consideration..."))
+Doses_administered: int = int(input("the total number of vaccine doses already given..."))
+Doses_per_day: int = int(input("Total number of doses given each day going forward..."))
+Target_percent_vaccinated: int = int(input("What percent of the population is the targetted goal?"))
+today: datetime = datetime.today()
+
+print(Population)
+print(Doses_administered)
+print(Doses_per_day)
+print(Target_percent_vaccinated)
