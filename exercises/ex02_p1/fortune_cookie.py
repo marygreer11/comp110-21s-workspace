19976193b9d84adfb14a69d5d342398b3bb6ffa5
Mary <maryag@email.unc.edu>
2021-02-09 21:51:44 -0500

    EX02 pt 1 draft 1

--- exercises/ex02_p1/fortune_cookie.py
+++ exercises/ex02_p1/fortune_cookie.py
@@ -1,18 +1,32 @@
 """Fortune cookie exercise redux as a structured program."""
 
 from random import randint
+from typing import ForwardRef
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730394883"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Your fortune cookie says...")
-    # TODO 2: Print the result of calling your fortune_cookie function.
+    print(fortune_cookie())
     print("Now, go spread positive vibes!")
+    return None
 
 
-# TODO 1: Define your fortune_cookie function here.
+
+def fortune_cookie() -> str:
+    """Declares fortune"""
+    Number: int =randint(1, 100)
+    if Number > 50:
+     return "You will have a good day"
+    if Number < 50:
+        return "Your dreams will come true"
+    if Number == 50:
+     return "Love is in the air"
+                
+            
+
 
 
 # Python Idiom for "starting" the program when run as a module.
--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -1,15 +1,24 @@
 """Tar Heels exercise redux as a structured program."""
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730394883"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
-    # TODO 2: Print the response of calling the tar_heels function here.
+    print(tar_heels(choice))
 
 
-# TODO 1: Define the tar_heels function, and its logic, here.
+def tar_heels(choice: int) -> str: 
+    """Tar heel fans function"""
+    choice: int = int(input("Enter an int: "))
+    if choice % 2 == 0 and choice %7 == 0:
+        return "TAR HEELS"
+    if choice % 2 == 0:
+        return "TAR"
+    if choice % 7 == 0:
+        return "HEELS"
+    return "CAROLINA"
 
 
 if __name__ == "__main__":
