a79779c36e880971952d036d342f4fbf58f9324f
Mary <maryag@email.unc.edu>
2021-02-24 18:13:42 -0500

    another cyoa

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -10,15 +10,15 @@ def main() -> None:
     cont: int = int(input("Would you like to play the game? Choose 1 to continue or choose 2 to quit. "))
     if cont == 1:
         answer1: int = int(input("What number will the generator give? 1 or 2? "))
-        actual1: int = randint(1,2)
-        points: int = round1(answer1, actual1)
+        actual1: int = randint(1, 2)
+        points1: int = round1(answer1, actual1)
         moving_on: int = int(input("Would you like to continue to round 2? Press 1 to continue and 2 to quit. "))
         if moving_on == 1:
             answer2: int = int(input("What number will the genorater give? Between 1-10. "))
-            actual2: int = randint(1,10)
-            points: int = (points)+ (round2(answer2, actual2))
+            actual2: int = randint(1, 10)
+            points: int = (points1) + (round2(answer2, actual2))
             NAMED_CONSTANT: str = "\U0001f600"
-            print(f"Congratulations, you have earned {points} points during the game. Come play again, {NAMED_CONSTANT}, !")
+            print(f"Congratulations, you have earned {points} points during the game.,{NAMED_CONSTANT}, !")
         else:
             if moving_on == 2:
                 print("Thank you for stopping by the Ultimate Guessing game! Your session has now ended.")
@@ -28,22 +28,22 @@ def main() -> None:
 
 
 def greet() -> None:
-    player: str = str(input("What is your name? "))
     """Greeting the player."""
+    player: str = str(input("What is your name? "))
     print(f"Hello {player}, welcome to the Ultimate Guessing Game!")
     
 
 def round1(answer1: int, actual1: int) -> int:
     """First round of the game."""
-    points1: int = 1 or 0
+    pointsr1: int = 1 or 0
     if answer1 == actual1:
-        points1 == 1
-        print(f"Congratulations, you have guessed correctly, you have earned, {points1} points this round.")
+        pointsr1 == 1
+        print(f"Congratulations, you have guessed correctly, you have earned, {pointsr1} points this round.")
     else:
         if answer1 != actual1:
-            points1 == 0
-            print(f"Unfortunately, you have not guessed correctly, you earned, {points1} points this round.")
-    return points1
+            pointsr1 == 0
+            print(f"Unfortunately, you have not guessed correctly, you earned, {pointsr1} points this round.")
+    return pointsr1
 
 
 def round2(answer2: int, actual2: int) -> int:
