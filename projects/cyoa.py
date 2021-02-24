8c1ebb09b89d2c12d9363a5e93f7208d35217b7e
Mary <maryag@email.unc.edu>
2021-02-24 18:06:43 -0500

    more cyoa

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -3,41 +3,38 @@
 __author__ = "730394883"
 
 from random import randint
-player: str = str(input("What is your name? "))
-answer1: int 
-actual1: int = randint(1,2)
-answer2: int 
-actual2: int = randint(1,10)
+
 def main() -> None:
-    """Where program will run"""
-    welcome: None = greet() 
-    print(welcome)
+    """Where program will run."""
+    greet()
     cont: int = int(input("Would you like to play the game? Choose 1 to continue or choose 2 to quit. "))
     if cont == 1:
         answer1: int = int(input("What number will the generator give? 1 or 2? "))
+        actual1: int = randint(1,2)
         points: int = round1(answer1, actual1)
         moving_on: int = int(input("Would you like to continue to round 2? Press 1 to continue and 2 to quit. "))
         if moving_on == 1:
             answer2: int = int(input("What number will the genorater give? Between 1-10. "))
+            actual2: int = randint(1,10)
             points: int = (points)+ (round2(answer2, actual2))
             NAMED_CONSTANT: str = "\U0001f600"
-            print(f"Congratulations, {player}, you have earned {points} points during the game. Come play again, {NAMED_CONSTANT}, !")
+            print(f"Congratulations, you have earned {points} points during the game. Come play again, {NAMED_CONSTANT}, !")
         else:
             if moving_on == 2:
                 print("Thank you for stopping by the Ultimate Guessing game! Your session has now ended.")
     else:
         if cont == 2:
             print("Thank you for stopping by the Ultimate Guessing game! Your session has now ended.")
-    return None
+
 
 def greet() -> None:
-    """Greeting the player"""
+    player: str = str(input("What is your name? "))
+    """Greeting the player."""
     print(f"Hello {player}, welcome to the Ultimate Guessing Game!")
-    return None
     
-def round1(Answer1: int, Actual1: int) -> int:
+
+def round1(answer1: int, actual1: int) -> int:
     """First round of the game."""
-    answer1: int = int(input("What number will the generator give? 1 or 2? "))
     points1: int = 1 or 0
     if answer1 == actual1:
         points1 == 1
@@ -48,9 +45,9 @@ def round1(Answer1: int, Actual1: int) -> int:
             print(f"Unfortunately, you have not guessed correctly, you earned, {points1} points this round.")
     return points1
 
-def round2(Answer2: int, Actual2: int) -> int:
+
+def round2(answer2: int, actual2: int) -> int:
     """Second round of the game."""
-    answer2: int = int(input("What number will the genorater give? Between 1-10. "))
     points2: int = 1 or 0 
     if answer2 == actual2:
         points2 = 1
@@ -61,6 +58,6 @@ def round2(Answer2: int, Actual2: int) -> int:
             print(f"Unfortunately, you have not guessed correctly. You have earned {points2} points this round.")
     return points2
 
+
 if __name__ == "__main__":
     main()
-    
