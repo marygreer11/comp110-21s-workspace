cfad3ffdf49df1d2d4287d5eaf383f67dba5d570
Mary <maryag@email.unc.edu>
2021-03-03 20:27:32 -0500

    palindromify draft 1

--- exercises/ex03/palindromify.py
+++ exercises/ex03/palindromify.py
@@ -1,12 +1,25 @@
 """EX03 - palindromify function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730394883"
 
 
 def main() -> None:
     """Entrypoint of the program."""
-    # Put print statements here to test your function
-    # ex. print(palindromify("race", false))
+    print(palindromify("race", False))
+    print(palindromify("red", True))
+
+
+def palindromify(x: str, y: bool) -> str:
+    """Mirroring words."""
+    if y == True:
+        middle: str = x[len(x)-1 // 2:(len(x) + 2) // 2]
+        final: str = (x[1:middle] + x[middle:len])
+    else:
+        if y == False:
+            middle: str = x[len(x)-1 // 2:(len(x) + 2) // 2 - 1]
+            final: str = (x[1: middle] + x[middle:len])
+    return final
+
 
 
 if __name__ == "__main__":
