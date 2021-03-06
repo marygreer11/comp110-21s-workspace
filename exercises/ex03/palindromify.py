6ec62d9881b4250a9652cc32fc830d9c9d777e93
Mary <maryag@email.unc.edu>
2021-03-05 22:01:32 -0500

    another ex03

--- exercises/ex03/palindromify.py
+++ exercises/ex03/palindromify.py
@@ -1,5 +1,8 @@
 """EX03 - palindromify function."""
 
+from typing import Final
+
+
 __author__: str = "730394883"
 
 
@@ -13,12 +16,12 @@ def palindromify(x: str, y: bool) -> str:
     """Mirroring words."""
     if y is True:
         middle: int = int(len(x) / 2)
-        final: str = (x[1: middle] + x[middle: len])
+        final = (x[1: middle] + x[middle: len])
     else:
         if y is False:
             mid: int = int(len(x) / 2) - 1
             final = (x[1: mid] + x[mid:len])
-    return final
+    return Final
     
 
 if __name__ == "__main__":
