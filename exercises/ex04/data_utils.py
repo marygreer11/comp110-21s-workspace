7b6ff00678a6226cdaef7d710ad4a056bc0c07a5
Mary <maryag@email.unc.edu>
2021-03-21 18:17:05 -0400

    read_csv_rows

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -1,6 +1,6 @@
 """Utility functions for wrangling data."""
 
-__author__ = "YOUR PID HERE"
+__author__ = "730394883"
 
 
 from csv import DictReader
@@ -9,10 +9,10 @@ from csv import DictReader
 def read_csv_rows(csv_file: str) -> list[dict[str, str]]:
     """Read a CSV file's contents into a list of rows."""
     rows: list[dict[str, str]] = []
-    
-    # TODO 0.1) Complete the implementation of this function here.
-
-    return rows
+    file_handle = open("nc_durham_2015_march_21_to_27.csv", "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        return rows
 
 
 # TODO: Define the other functions here.
