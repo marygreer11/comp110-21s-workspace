d4e09bc20ad1d80deab421aa427f62ff42ba4399
Mary <maryag@email.unc.edu>
2021-03-21 18:36:56 -0400

    new file name

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -9,7 +9,7 @@ from csv import DictReader
 def read_csv_rows(csv_file: str) -> list[dict[str, str]]:
     """Read a CSV file's contents into a list of rows."""
     rows: list[dict[str, str]] = []
-    file_handle = open("data/nc_durham_2015_march_21_to_27.csv", "r", encoding="utf8")
+    file_handle = open("data/trafficdata.csv", "r", encoding="utf8")
     csv_reader = DictReader(file_handle)
     for row in csv_reader:
         rows.append(row)
