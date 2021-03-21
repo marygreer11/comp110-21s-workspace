6ebeb1a5fb32e8dae46adb2633fe5a4306228ad6
Mary <maryag@email.unc.edu>
2021-03-21 18:22:52 -0400

    read_csv_rows

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -12,7 +12,8 @@ def read_csv_rows(csv_file: str) -> list[dict[str, str]]:
     file_handle = open("nc_durham_2015_march_21_to_27.csv", "r", encoding="utf8")
     csv_reader = DictReader(file_handle)
     for row in csv_reader:
-        return rows
+        rows.append(row)
+    return rows
 
 
 # TODO: Define the other functions here.
